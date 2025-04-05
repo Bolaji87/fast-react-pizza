6dd15e5a180198921799145db902bde7c4ef3999
Bolaji87 <sakabolaji77@gmail.com>
2025-04-05 09:55:47 +0100

    feat:update

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,7 +1,22 @@
+/* eslint-disable react/prop-types */
 import React from "react";
+import { Link } from "react-router-dom";
 
-function Button() {
-  return <div>Button</div>;
+function Button({ children, disabled, to, onclick }) {
+  const className =
+    "inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-4";
+
+  if (to)
+    return (
+      <Link className={className} to="/order/new">
+        {children}
+      </Link>
+    );
+  return (
+    <button onClick={onclick} to={to} disabled={disabled} className={className}>
+      {children}
+    </button>
+  );
 }
 
 export default Button;
