45514ca5193925081a81ab0188fef4b2e28479cd
Bolaji87 <sakabolaji77@gmail.com>
2025-04-08 03:06:15 +0100

    feat:create a round style type for updatequantitybutton

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -8,6 +8,7 @@ function Button({ children, disabled, to, onClick, type }) {
   const style = {
     primary: base + " px-4 py-3 md:px-6 md:py-4 ",
     small: base + " py-2 px-4 md:px-5 md:py-2.5 text-xs",
+    round: base + " px-2.5 py-1 md:px-3.5 md:py-2 text-sm",
     secondary:
       "inline-block text-sm px-4 py-2.5 md:px-6 md:py-3.5 hover:text-stone-800 focus:text-stone-800 rounded-full border-2 border-stone-300 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-stone-300 focus:bg-stone-200 focus:outline-none focus:ring focus:ring-stone-300 focus:ring-offset-2 disabled:cursor-not-allowed",
   };
