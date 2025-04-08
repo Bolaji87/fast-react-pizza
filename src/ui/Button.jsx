71bcc668f13c95f65f6abe75a184ffb3d53e9d89
Bolaji87 <sakabolaji77@gmail.com>
2025-04-08 01:10:24 +0100

    feat:update Button with onclick prop

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-function Button({ children, disabled, to, onclick, type }) {
+function Button({ children, disabled, to, onClick, type }) {
   const base =
     "inline-block rounded-full text-sm bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed";
   const style = {
@@ -17,13 +17,21 @@ function Button({ children, disabled, to, onclick, type }) {
         {children}
       </Link>
     );
+
+  if (onClick)
+    return (
+      <button
+        to={to}
+        onClick={onClick}
+        disabled={disabled}
+        className={style[type]}
+      >
+        {children}
+      </button>
+    );
+
   return (
-    <button
-      onClick={onclick}
-      to={to}
-      disabled={disabled}
-      className={style[type]}
-    >
+    <button to={to} disabled={disabled} className={style[type]}>
       {children}
     </button>
   );
