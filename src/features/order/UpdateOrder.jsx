64f11e9eeaeb6bb38263d8a36158e497fbe3aaad
Bolaji87 <sakabolaji77@gmail.com>
2025-04-09 01:12:21 +0100

    feat:call useFetcher hook with fetcher.Form

--- src/features/order/UpdateOrder.jsx
+++ src/features/order/UpdateOrder.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import Button from "../../ui/Button";
+import { useFetcher } from "react-router-dom";
 
 function UpdateOrder({ order }) {
-  return <Button type="primary">Make Priority</Button>;
+  const fetcher = useFetcher();
+
+  return (
+    <fetcher.Form method="PATCH" className="text-right">
+      <Button type="primary">Make Priority</Button>
+    </fetcher.Form>
+  );
 }
 
 export default UpdateOrder;
