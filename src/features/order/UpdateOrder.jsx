64c00531619cc684f42afdc109329fc34f571eae
Bolaji87 <sakabolaji77@gmail.com>
2025-04-09 01:05:11 +0100

    feat:create UpdateOrder

---
+++ src/features/order/UpdateOrder.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Button from "../../ui/Button";
+
+function UpdateOrder({ order }) {
+  return <Button type="primary">Make Priority</Button>;
+}
+
+export default UpdateOrder;
